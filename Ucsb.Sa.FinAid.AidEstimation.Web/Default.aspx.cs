9f9ecc3aee1059e9097cdc0e10fb7bb80f75957c
Logan Franken <loganfranken@gmail.com>
2013-11-26 13:52:07 -0800

    Added sample web interface project

---
+++ Ucsb.Sa.FinAid.AidEstimation.Web/Default.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ucsb.Sa.FinAid.AidEstimation.Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
