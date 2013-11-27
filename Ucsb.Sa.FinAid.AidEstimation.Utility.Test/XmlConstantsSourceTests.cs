b529dc091e101c37e5569e2797fc8847be1877bb
Logan Franken <loganfranken@gmail.com>
2013-11-26 16:25:11 -0800

    Added utility for reading constants from XML file

---
+++ Ucsb.Sa.FinAid.AidEstimation.Utility.Test/XmlConstantsSourceTests.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ucsb.Sa.FinAid.AidEstimation.EfcCalculation;
+
+namespace Ucsb.Sa.FinAid.AidEstimation.Utility.Test
+{
+    [TestClass]
+    public class XmlConstantsSourceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void StringConstructor_NullValue_ThrowsException()
+        {
+            string sourcePath = null;
+            XmlConstantsSource source = new XmlConstantsSource(sourcePath);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void StringConstructor_EmptyString_ThrowsException()
+        {
+            XmlConstantsSource source = new XmlConstantsSource(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void StringConstructor_FileDoesNotExist_ThrowsException()
+        {
+            XmlConstantsSource source = new XmlConstantsSource("this_file_does_not_exist.xml");
+        }
+
+        [TestMethod]
+        public void StringConstructor_FileExists_Succeeds()
+        {
+            XmlConstantsSource source = new XmlConstantsSource("TestFile.xml");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void XmlDocConstructor_NullValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = null;
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+        }
+
+        [TestMethod]
+        public void XmlDocConstructor_XmlDoc_Succeeds()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetValue_NullValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetValue<string>(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetValue_EmptyValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetValue<string>(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetValue_NoNode_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = "<constants></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetValue<string>("doesntexist");
+        }
+
+        [TestMethod]
+        public void GetValue_IntValue_ReturnsInt()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""><value>3</value></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            int value = source.GetValue<int>("test");
+
+            Assert.AreEqual(3, value);
+        }
+
+        [TestMethod]
+        public void GetValue_EnumValue_ReturnsEnum()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""><value>California</value></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            UnitedStatesStateOrTerritory value = source.GetValue<UnitedStatesStateOrTerritory>("test");
+
+            Assert.AreEqual(UnitedStatesStateOrTerritory.California, value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetValue_NoValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetValue<int>("test");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetArray_NullValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetArray<string>(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetArray_EmptyValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetArray<string>(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetArray_NoNode_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = "<constants></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetArray<string>("doesntexist");
+        }
+
+        [TestMethod]
+        public void GetArray_IntValue_ReturnsInt()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml =
+                    @"<constants><constant name=""test""><values><value>1</value><value>2</value><value>3</value></values></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            int[] values = source.GetArray<int>("test");
+
+            Assert.AreEqual(2, values[1]);
+            Assert.AreEqual(3, values.Length);
+        }
+
+        [TestMethod]
+        public void GetArray_EnumValue_ReturnsEnum()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml =
+                    @"<constants><constant name=""test""><values><value>California</value><value>Texas</value></values></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            UnitedStatesStateOrTerritory[] values = source.GetArray<UnitedStatesStateOrTerritory>("test");
+
+            Assert.AreEqual(UnitedStatesStateOrTerritory.Texas, values[1]);
+            Assert.AreEqual(2, values.Length);
+        }
+
+        [TestMethod]
+        public void GetArray_EmptyValues_EmptyArray()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""><values /></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            int[] values = source.GetArray<int>("test");
+
+            Assert.AreEqual(0, values.Length);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetArray_NoValues_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetArray<int>("test");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetMultiArray_NullValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetMultiArray<string>(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetMultiArray_EmptyValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetMultiArray<string>(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetMultiArray_NoNode_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = "<constants></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetMultiArray<string>("doesntexist");
+        }
+
+        [TestMethod]
+        public void GetMultiArray_IntValue_ReturnsInt()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml =
+                    @"<constants><constant name=""test""><values><value>1</value><value>2</value></values><values><value>3</value></values></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            int[,] values = source.GetMultiArray<int>("test");
+
+            Assert.AreEqual(2, values.Rank);
+            Assert.AreEqual(2, values[0, 1]);
+        }
+
+        [TestMethod]
+        public void GetMultiArray_EnumValue_ReturnsEnum()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml =
+                    @"<constants><constant name=""test""><values><value>California</value></values><values><value>Texas</value></values></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            UnitedStatesStateOrTerritory[,] values = source.GetMultiArray<UnitedStatesStateOrTerritory>("test");
+
+            Assert.AreEqual(UnitedStatesStateOrTerritory.Texas, values[1, 0]);
+        }
+
+        [TestMethod]
+        public void GetMultiArray_EmptyValues_EmptyArray()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""><values /></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            int[,] values = source.GetMultiArray<int>("test");
+
+            Assert.AreEqual(values.Length, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetMultiArray_NoValues_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetArray<int>("test");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetKeyValuePairArray_NullValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetKeyValuePairArray<string>(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetKeyValuePairArray_EmptyValue_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetKeyValuePairArray<string>(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetKeyValuePairArray_NoNode_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = "<constants></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            source.GetMultiArray<string>("doesntexist");
+        }
+
+        [TestMethod]
+        public void GetKeyValuePairArray_StringValue_ReturnsString()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml =
+                    @"<constants><constant name=""test""><value key=""testkey"">testvalue</value></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            KeyValuePair<string, string>[] values = source.GetKeyValuePairArray<string>("test");
+
+            Assert.AreEqual(1, values.Length);
+            Assert.AreEqual("testvalue", values.Where(v => v.Key == "testkey").FirstOrDefault().Value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void GetKeyValuePairArray_NoKey_ThrowsException()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""><value>testvalue</value></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            KeyValuePair<string, string>[] values = source.GetKeyValuePairArray<string>("test");
+        }
+
+        [TestMethod]
+        public void GetKeyValuePairArray_EnumValue_ReturnsEnum()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml =
+                    @"<constants><constant name=""test""><value key=""state"">Minnesota</value></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            KeyValuePair<string, UnitedStatesStateOrTerritory>[] values
+                = source.GetKeyValuePairArray<UnitedStatesStateOrTerritory>("test");
+
+            Assert.AreEqual(UnitedStatesStateOrTerritory.Minnesota, values[0].Value);
+        }
+
+        [TestMethod]
+        public void GetKeyValuePairArray_NoValues_EmptyArray()
+        {
+            XmlDocument xmlDoc = new XmlDocument
+            {
+                InnerXml = @"<constants><constant name=""test""></constant></constants>"
+            };
+
+            XmlConstantsSource source = new XmlConstantsSource(xmlDoc);
+            KeyValuePair<string, int>[] values = source.GetKeyValuePairArray<int>("test");
+
+            Assert.AreEqual(values.Length, 0);
+        }
+    }
+}
---
+++ Ucsb.Sa.FinAid.AidEstimation.Utility/XmlConstantsSource.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Ucsb.Sa.FinAid.AidEstimation.Utility
+{
+    /// <summary>
+    /// Reads constants used within Aid Estimation calculations from XML files
+    /// </summary>
+    public class XmlConstantsSource
+    {
+        private enum ConstantMultiplicity
+        {
+            Single,
+            Multiple,
+            Multidimensional
+        }
+
+        private readonly XmlDocument _source;
+
+        /// <summary>
+        /// Creates a new constants source with values harvested from the XML file
+        /// at the specified path
+        /// </summary>
+        /// <param name="sourcePath">Full path to the source file</param>
+        public XmlConstantsSource(string sourcePath)
+        {
+            if (String.IsNullOrEmpty(sourcePath))
+            {
+                throw new ArgumentException("Path for XML source file was not provided");
+            }
+
+            if (!File.Exists(sourcePath))
+            {
+                throw new ArgumentException("XML source file does not exist");
+            }
+
+            _source = new XmlDocument();
+            _source.Load(sourcePath);
+        }
+
+        /// <summary>
+        /// Creates a new constants source with values harvested from the <see cref="XmlDocument"/>
+        /// </summary>
+        /// <param name="source">Source XML</param>
+        public XmlConstantsSource(XmlDocument source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentException();
+            }
+
+            _source = source;
+        }
+
+        /// <summary>
+        /// Returns the value of a constant
+        /// </summary>
+        /// <typeparam name="T">Type of the constant</typeparam>
+        /// <param name="constantName">Name of the constant</param>
+        /// <returns>The value of the constant</returns>
+        public T GetValue<T>(string constantName)
+        {
+            XmlNode node = GetConstantNode(constantName, ConstantMultiplicity.Single);
+            return ConvertValue<T>(node.InnerText);
+        }
+
+        /// <summary>
+        /// Returns the values of a constant, contained within an array
+        /// </summary>
+        /// <typeparam name="T">Type of the constant</typeparam>
+        /// <param name="constantName">Name of the constant</param>
+        /// <returns>The values of the constant, as an array</returns>
+        public T[] GetArray<T>(string constantName)
+        {
+            XmlNode node = GetConstantNode(constantName, ConstantMultiplicity.Multiple);
+
+            T[] constants = new T[node.ChildNodes.Count];
+            for (int i = 0; i < node.ChildNodes.Count; i++)
+            {
+                constants[i] = ConvertValue<T>(node.ChildNodes[i].InnerText);
+            }
+
+            return constants;
+        }
+
+        /// <summary>
+        /// Returns the values of a constant, as a multidimensional array
+        /// </summary>
+        /// <typeparam name="T">Type of the constant</typeparam>
+        /// <param name="constantName">Name of the constant</param>
+        /// <returns>The values of the constant, as a multidimensional array</returns>
+        public T[,] GetMultiArray<T>(string constantName)
+        {
+            XmlNode node = GetConstantNode(constantName, ConstantMultiplicity.Multidimensional);
+
+            T[,] constants = new T[node.ChildNodes.Count, node.ChildNodes[0].ChildNodes.Count];
+            for (int i = 0; i < node.ChildNodes.Count; i++)
+            {
+                for (int j = 0; j < node.ChildNodes[i].ChildNodes.Count; j++)
+                {
+                    constants[i, j] = ConvertValue<T>(node.ChildNodes[i].ChildNodes[j].InnerText);
+                }
+            }
+
+            return constants;
+        }
+
+        /// <summary>
+        /// Returns the value of a constant, as a key value pair
+        /// </summary>
+        /// <typeparam name="T">Type of the constant</typeparam>
+        /// <param name="constantName">Name of the constant</param>
+        /// <returns>The values of the constant, as a key value pair</returns>
+        public KeyValuePair<string, T>[] GetKeyValuePairArray<T>(string constantName)
+        {
+            XmlNode node = GetConstantNode(constantName, ConstantMultiplicity.Multidimensional);
+            KeyValuePair<string, T>[] constants = new KeyValuePair<string, T>[node.ChildNodes.Count];
+
+            for (int i = 0; i < node.ChildNodes.Count; i++)
+            {
+                if (node.ChildNodes[i].Attributes == null
+                    || node.ChildNodes[i].Attributes["key"] == null)
+                {
+                    throw new ArgumentException("No key provided");
+                }
+
+                string key = node.ChildNodes[i].Attributes["key"].Value;
+                T value = ConvertValue<T>(node.ChildNodes[i].InnerText);
+                constants[i] = new KeyValuePair<string, T>(key, value);
+            }
+
+            return constants;
+        }
+
+        private XmlNode GetConstantNode(string constantName, ConstantMultiplicity multiplicity)
+        {
+            if (String.IsNullOrEmpty(constantName))
+            {
+                throw new ArgumentException("No constant name provided");
+            }
+
+            string valueSelector = null;
+
+            switch (multiplicity)
+            {
+                case ConstantMultiplicity.Single:
+                    valueSelector = "/value";
+                    break;
+
+                case ConstantMultiplicity.Multiple:
+                    valueSelector = "/values";
+                    break;
+
+                case ConstantMultiplicity.Multidimensional:
+                    valueSelector = String.Empty;
+                    break;
+            }
+
+            string selectionString = GetSelectionString(constantName, valueSelector);
+            XmlNode node = _source.SelectSingleNode(selectionString);
+
+            if (node == null)
+            {
+                throw new ArgumentException("Constant could not be found");
+            }
+
+            return node;
+        }
+
+        private static T ConvertValue<T>(string value)
+        {
+            Type constantType = typeof(T);
+
+            if (constantType.BaseType == typeof(Enum))
+            {
+                return (T)Enum.Parse(constantType, value);
+            }
+
+            return (T)Convert.ChangeType(value, constantType);
+        }
+
+        private static string GetSelectionString(string constantName, string valueSelector)
+        {
+            return String.Format("constants/constant[@name='{0}']{1}", constantName, valueSelector);
+        }
+    }
+}
