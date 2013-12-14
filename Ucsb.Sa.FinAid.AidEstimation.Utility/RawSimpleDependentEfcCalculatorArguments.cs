3a7baf5dec7b2de94642f94501c495af5d100093
Logan Franken <loganfranken@gmail.com>
2013-12-13 16:46:59 -0800

    Added arguments for simplified equation

---
+++ Ucsb.Sa.FinAid.AidEstimation.Utility/RawSimpleDependentEfcCalculatorArguments.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ucsb.Sa.FinAid.AidEstimation.Utility
+{
+    public class RawSimpleDependentEfcCalculatorArguments
+    {
+        public string MaritalStatus { get; set; }
+
+        public string OldestParentAge { get; set; }
+
+        public string ParentIncome { get; set; }
+
+        public string ParentOtherIncome { get; set; }
+
+        public string ParentIncomeEarnedBy { get; set; }
+
+        public string ParentIncomeTax { get; set; }
+
+        public string ParentAssets { get; set; }
+
+        public string StudentIncome { get; set; }
+
+        public string StudentOtherIncome { get; set; }
+
+        public string StudentIncomeTax { get; set; }
+
+        public string StudentAssets { get; set; }
+
+        public string NumberInHousehold { get; set; }
+
+        public string NumberInCollege { get; set; }
+
+        public string StateOfResidency { get; set; }
+    }
+}
---
+++ Ucsb.Sa.FinAid.AidEstimation.Utility/RawSimpleIndependentEfcCalculatorArguments.cs
@@ -0,0 +1,27 @@
+﻿namespace Ucsb.Sa.FinAid.AidEstimation.Utility
+{
+    public class RawSimpleIndependentEfcCalculatorArguments
+    {
+        public string MaritalStatus { get; set; }
+
+        public string StudentAge { get; set; }
+
+        public string StudentIncome { get; set; }
+
+        public string StudentOtherIncome { get; set; }
+
+        public string StudentIncomeEarnedBy { get; set; }
+
+        public string StudentIncomeTax { get; set; }
+
+        public string StudentAssets { get; set; }
+
+        public string NumberInHousehold { get; set; }
+
+        public string NumberInCollege { get; set; }
+
+        public string HasDependents { get; set; }
+
+        public string StateOfResidency { get; set; }
+    }
+}
