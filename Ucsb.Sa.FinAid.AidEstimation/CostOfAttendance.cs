6df50be8a5acd1c08c366a20d298751b884c5fff
Logan Franken <loganfranken@gmail.com>
2014-01-08 17:07:24 -0800

    Added Out of State Fees to COA class

--- Ucsb.Sa.FinAid.AidEstimation/CostOfAttendance.cs
+++ Ucsb.Sa.FinAid.AidEstimation/CostOfAttendance.cs
@@ -5,11 +5,21 @@ namespace Ucsb.Sa.FinAid.AidEstimation
 {
     public class CostOfAttendance
     {
+        public double OutOfStateFees { get; set; }
+
+        /// <summary>
+        /// The list of items that constitute the Cost of Attendance.
+        /// This does not include Out of State Fees
+        /// </summary>
         public List<CostOfAttendanceItem> Items
         {
             get;
             private set;
         }
+
+        /// <summary>
+        /// Total Cost of Attendance (not including Out of State Fees)
+        /// </summary>
         public double Total
         {
             get
@@ -17,9 +27,17 @@ namespace Ucsb.Sa.FinAid.AidEstimation
                 return Items.Sum(item => item.Value);
             }
         }
-        public CostOfAttendance(params CostOfAttendanceItem[] items) : this()
+
+        public CostOfAttendance(params CostOfAttendanceItem[] items)
+            : this()
         {
             Items.AddRange(items);
+        }
+
+        public CostOfAttendance(double outOfStateFees, params CostOfAttendanceItem[] items)
+            : this(items)
+        {
+            OutOfStateFees = outOfStateFees;
         }
 
         public CostOfAttendance()
