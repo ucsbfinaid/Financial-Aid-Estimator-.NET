d7b173c826929d5a86bd75155b989b1d975881c5
Logan Franken <loganfranken@gmail.com>
2014-01-15 12:51:45 -0800

    Added AwardRange

---
+++ Ucsb.Sa.FinAid.AidEstimation.Test/AwardRangeTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ucsb.Sa.FinAid.AidEstimation.Test
+{
+    [TestClass]
+    public class AwardRangeTests
+    {
+        [TestMethod]
+        public void Constructor_ValidValues_Constructed()
+        {
+            AwardRange range = new AwardRange(500, 1000);
+            Assert.AreEqual(500, range.Minimum);
+            Assert.AreEqual(1000, range.Maximum);
+        }
+
+        [TestMethod]
+        public void Constructor_NegativeNumber_EqualsZero()
+        {
+            AwardRange range = new AwardRange(-1000, 100);
+            Assert.AreEqual(0, range.Minimum);
+            Assert.AreEqual(100, range.Maximum);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_MinimumGreaterThanMaximum_ThrowsException()
+        {
+            AwardRange range = new AwardRange(500, 0);
+        }
+
+        [TestMethod]
+        public void ToString_DifferentValues_OutputsRangeString()
+        {
+            AwardRange range = new AwardRange(500, 600);
+            Assert.AreEqual("$500.00 - $600.00", range.ToString());
+        }
+
+        [TestMethod]
+        public void ToString_EqualValues_OutputsSingleValueString()
+        {
+            AwardRange range = new AwardRange(500, 500);
+            Assert.AreEqual("$500.00", range.ToString());
+        }
+
+        [TestMethod]
+        public void GetRangeFromValue_SingleValidBuffer_CreatesRange()
+        {
+            AwardRange range = AwardRange.GetRangeFromValue(500, 100);
+            Assert.AreEqual(400, range.Minimum);
+            Assert.AreEqual(600, range.Maximum);
+        }
+
+        [TestMethod]
+        public void GetRangeFromValue_MultipleValidBuffers_CreatesRange()
+        {
+            AwardRange range = AwardRange.GetRangeFromValue(500, 50, 200);
+            Assert.AreEqual(450, range.Minimum);
+            Assert.AreEqual(700, range.Maximum);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetRangeFromValue_FirstNegativeBufferValue_ThrowsException()
+        {
+            AwardRange.GetRangeFromValue(400, -100, 500);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetRangeFromValue_FirstSecondBufferValue_ThrowsException()
+        {
+            AwardRange.GetRangeFromValue(400, 100, -500);
+        }
+    }
+}
---
+++ Ucsb.Sa.FinAid.AidEstimation/AwardRange.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Ucsb.Sa.FinAid.AidEstimation
+{
+    /// <summary>
+    /// Represents a ranged Financial Aid award. For example: "$500 - $700".
+    /// Negative amounts are not allowed within a range; negative amounts will be
+    /// set to zero
+    /// </summary>
+    public class AwardRange
+    {
+        public double Maximum
+        {
+            get;
+            set;
+        }
+
+        public double Minimum
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Creates a range using the provided target value and buffer value. For example,
+        /// if the "value" is "500" and the "buffer" is "200", the resulting range will be
+        /// "$300 - $700". If, after the buffer calculation, either of the range values are
+        /// negative, they will be set to zero
+        /// </summary>
+        public static AwardRange GetRangeFromValue(double value, double buffer)
+        {
+            return GetRangeFromValue(value, buffer, buffer);
+        }
+
+        /// <summary>
+        /// Creates a range using the provided target value and buffer values. For example,
+        /// if the "value" is "500", the "lowerBuffer" is "300", and the "upperBuffer" is
+        /// "100", the resulting range will be "$200 - $600". If, after the buffer calculation,
+        /// either of the range values are negative, they will be set to zero
+        /// </summary>
+        public static AwardRange GetRangeFromValue(double value, double lowerBuffer, double upperBuffer)
+        {
+            if (lowerBuffer < 0 || upperBuffer < 0)
+            {
+                throw new ArgumentException("Buffer values can not be negative");
+            }
+
+            return new AwardRange(value - lowerBuffer, value + upperBuffer);
+        }
+
+        public AwardRange(double minimum, double maximum)
+        {
+            if (minimum > maximum)
+            {
+                throw new ArgumentException("Minimum range value can not be greater than maximum range value");
+            }
+
+            Minimum = (minimum <= 0) ? 0 : minimum;
+            Maximum = (maximum <= 0) ? 0 : maximum;
+        }
+
+        /// <summary>
+        /// Displays the range in the following format: "${Minimum} - ${Maximum}". If the two amounts
+        /// are equal, only a single amount displays
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return (Minimum == Maximum)
+                ? Minimum.ToString("C")
+                : String.Format("{0} - {1}", Minimum.ToString("C"), Maximum.ToString("C"));
+        }
+    }
+}
